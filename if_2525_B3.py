60a24c6041e22b2b1fc06f21c39d85fe94648498
HelloSleep <nargene@gmail.com>
2023-01-01 04:47:31 +0900

    Update if_2525_B3.py

--- if_2525_B3.py
+++ if_2525_B3.py
@@ -1,11 +1,11 @@
 h, m = map(int, input().split())
 n    = int(input())
 if n+m>59:
-    h+= (m+n)//60 # keypoint
-    m = (m+n)%60 # keypoint
+    h+= (m+n)//60 # keypoint1
+    m = (m+n)%60 # keypoint2
 else:
     m += n
   
 if h>23:
-    h=h%24 # keypoint
+    h=h%24 # keypoint3
 print(h,m)
