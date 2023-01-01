b58a70baa5dab318e4e07e5e5a706bd5744089b2
HelloSleep <nargene@gmail.com>
2023-01-01 16:56:35 +0900

    Update for_1110_B1.py

--- for_1110_B1.py
+++ for_1110_B1.py
@@ -23,3 +23,24 @@ while num != n:
     cycle+=1
     
 print(cycle)
+
+---
+#I was obsessed with the explanation of the problem of a number less than 10, so I couldn't properly set the condition of the while statement,
+#this makes too much clone
+#modified
+n = int(input())
+num = n
+cycle = 0
+
+while True:
+  a = num//10
+  b = num%10
+  c = (a+b)%10
+  num = b*10+ c
+  
+  cycle +=1
+  if(num ==n):
+    break
+    
+print(cycle)
+
