a7a954b02116adb93b2b2851077308818be4d221
HelloSleep <nargene@gmail.com>
2023-01-23 14:32:58 +0000

    다리를지나는트럭 문제 수정

--- lvl2/stack-queue_다리를지나는트럭_X.py
+++ lvl2/stack-queue_다리를지나는트럭_X.py
@@ -23,3 +23,32 @@ def solution(bridge_length, weight, truck_weights):
                 bridge.append(0)
 
     return answer
+
+
+# 기존의 코드는 sum함수 때문에 시간이 오래걸림.
+# sum 대신 변수를 통해 현재까지 다리를 지나는 트럭을 저장해서 사용해주면 시간초과해결 가능
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = [0 for _ in range(bridge_length)]
+    
+    truck_sum = 0 # sum함수 시간 초과를 제거하기 위해 변수 사용
+
+    while bridge:
+        answer += 1
+
+        temp1 = bridge.pop(0) # bridge에서 사라지는 트럭 무게 제거
+        truck_sum -= temp1 
+
+        if truck_weights:
+            if truck_sum + truck_weights[0] <= weight:
+                
+                temp2 = truck_weights.pop(0) # bridge에서 추가되는 트럭 무게 추가
+                truck_sum += temp2
+                
+                bridge.append(temp2)
+            else:
+                bridge.append(0)
+
+    return answer
